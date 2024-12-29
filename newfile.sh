860ff6137e0b6384dce383f236aa9d9eedc2488c
sharanya123-ty <sharavin422@gmail.com>
2024-12-29 20:06:13 +0530

    Update newfile.sh

--- newfile.sh
+++ newfile.sh
@@ -1,7 +1,8 @@
-
 ls 
-if [ -d hello-world-war ] then rm -rf * 
+if [ -d hello-world-war ]
+then
+   rm -rf * 
 fi
- git clone https://github.com/sharanya123-ty/hello-world-war.git 
+ git clone https://github.com/sharanya123-ty/hello-world-war.git
 cd hello-world-war
  mvn clean package
